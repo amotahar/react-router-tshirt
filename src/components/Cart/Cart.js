ba62a27075cf070d809cf76e5e3538b0beb96e4b
atuhin <atuhinbd@gmail.com>
2023-01-21 21:37:54 +0600

    fdgfsdgfdsg

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart, handleRemoveItem}) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please select minimum an item!!!</p>
+    } 
+    else if(cart.length === 1){
+            message = <div>
+            <h3>Amr jonno ekta nao</h3>
+            <p>Tomar nijer jonno ekta naw</p>
+            <p><small>Amr jonno ekta naw please!</small></p>
+        </div>
+    }
     return (
         <div>
-            <h3>Order Summary </h3>
-            <h5>Order Quantity: {cart.length}</h5>
+            <h3 className={cart.length ===2 ? 'orange' : 'purple'}>Order Summary </h3>
+            <h5 className={`bold ${cart.length ===2 ? 'blue': 'yellow'}`}>Order Quantity: {cart.length}</h5>
             {
                 cart.map(tshirt => <p
                 key={tshirt._id}>
@@ -13,6 +25,19 @@ const Cart = ({cart, handleRemoveItem}) => {
 
                 <button onClick={()=>handleRemoveItem(tshirt)} >X</button>
                 </p> )
+            }{
+                message
+            }
+            {
+                cart.length ===3 ?<p>tin jon k gift diba?</p> : <p> Kino kino kino</p>
+            }
+            <p>and operator</p>
+            {
+                cart.length===2 && <h2>Double Item</h2>
+            }
+            <p>Or operator</p>
+            {
+                cart.length ===4 || <h2>Charta Item naw</h2>
             }
         </div>
     );
